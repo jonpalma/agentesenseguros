4107f286f32e7eb186bbc32862b3c2cf5304d15b
PedroBorundaMixen <pedro.borunda@mixen.mx>
2016-11-30 17:18:22 -0700

    Site Map y Meta Tags

--- header.php
+++ header.php
@@ -3,10 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Agentes / Consultores en Seguros</title>
+        <meta name="description" content="En Agentes / Consultores nos ocupamos en brindarte un excelente servicio para que estés tranquilo en las decisiones más importantes de tu vida. Cuidamos de ti en todos los aspectos ya que tú y tu familia son lo más importante para nosotros.">
+        <meta name="keywords" content="agentes,seguros,casa,auto,vida,gastos,medicos,mayores,cotiza,servicio,cuidado,asegurar,">
+        <meta name="author" content="Mixen">
         <link rel="shortcut icon" type="image/png" href="img/favicon.ico"/>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
+        <title>Agentes / Consultores en Seguros</title>
     </head>
     <body>
         <nav class="navbar navbar-default navbar-top">
@@ -23,7 +26,6 @@
                         <img src="img/logo.png" alt="Logo">
                     </a>
                 </div>
-
                 <div class="collapse navbar-collapse" id="navbar-collapse">
                     <ul class="nav navbar-nav pull-right">
                         <li class="active"><a href="#inicio" class="smoothScroll">INICIO</a></li>
